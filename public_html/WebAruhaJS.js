f8f551f40b8b00e45329435a2f7193b2f7a17db1
T.Ádám <tellad.adam@gmail.com>
2021-05-18 23:13:12 +0200

    3. feladat

--- public_html/WebAruhaJS.js
+++ public_html/WebAruhaJS.js
@@ -2,5 +2,7 @@ $(function () {
    
 
 });
+var szovegJSON =['[{"nev":"Porcelán pohárt","allapot":"Újra csomagolt","ara":"1500ft","darab":"10"},{"nev":"Porcelán tányér","allapot":"Újra csomagolt","ara":"1300ft","darab":"20"},{"nev":"Kanál","allapot":"Újra csomagolt","ara":"1200ft","darab":"50"}]'];
+
 
 
